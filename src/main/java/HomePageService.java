9287c12d5d2873571fb97aa01f089c23c776584a
Keno-AI <kenessary.myrzakul@nu.edu.kz>
2019-10-16 21:39:24 +0600

    Edited request ajax in index/html file

--- src/main/java/HomePageService.java
+++ src/main/java/HomePageService.java
@@ -1,13 +1,22 @@
+import com.google.gson.Gson;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/index")
 public class HomePageService {
     @GET
-    @Produces("text/html")
-    public String getHtml() {
-        return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
+    public Response getMyList(@QueryParam("from") String from, @QueryParam("to") String to,@QueryParam("date") String date) {
+        //Gson gson = new Gson();
+        String outcome= from+to+date;
+
+
+
+        //json = gson.toJson();
+
+        return Response.ok(outcome).build();
     }
 }
